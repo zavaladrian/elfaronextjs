94ccaad3971816ae85ee9b4cd06a9eccb7b96e60
zavaladrian <zavaladrian@gmail.com>
2024-11-29 20:49:20 -0600

    added tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,12 @@ import { Inter } from "next/font/google";
 import "./main.css";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "El Faro Restuarants",
-  description: "Website made for El Faro El Faro",
+  description: "Website made for El Faro Summit and  El Faro 2 Riverside",
 };
 
 export default function RootLayout({
@@ -19,11 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
+      <head> 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QFVF6773D5">
+</script>
+         </head>
       <body className={inter.className}>
     <Navbar />
         {children}
     <Footer />
+    <Analytics />
         </body>
     </html>
   );
